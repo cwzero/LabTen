930ef48a14f409910f2eedc2b38d3a617f6a106c
Eric Gjerde <gjerdeeric@gmail.com>
2015-11-12 17:59:01 -0600

    Optimize shape test class

--- src/edu/nwtc/java/ShapeTest.java
+++ src/edu/nwtc/java/ShapeTest.java
@@ -11,20 +11,18 @@ import edu.nwtc.java.twod.TwoDimensionalShape;
 
 public class ShapeTest {
 	public static void main(String[] args) {
-		TwoDimensionalShape[] twoD = {new Circle(3.0), new Square(3.0), new Triangle(3.0)};
-		ThreeDimensionalShape[] threeD = {new Cube(3.0), new Sphere(3.0), new Tetrahedron(3.0)};
+		Shape[] shapes = {new Circle(3.0), new Square(3.0), new Triangle(3.0), new Cube(3.0), new Sphere(3.0), new Tetrahedron(3.0)};
 
-		for (TwoDimensionalShape shape: twoD) {
+		for (Shape shape: shapes) {
 			String shapeName = shape.getClass().getName();
 			shapeName = shapeName.substring(shapeName.lastIndexOf(".") + 1);
-			System.out.println(shapeName + ": Area: " + shape.calcArea());
-		}
-
-		for (ThreeDimensionalShape shape: threeD) {
-			String shapeName = shape.getClass().getName();
-			shapeName = shapeName.substring(shapeName.lastIndexOf(".") + 1);
-			System.out.println(shapeName + ": SurfaceArea: " + shape.calcArea());
-			System.out.println(shapeName + ": Volume: " + shape.calcVolume());
+			if (shape instanceof TwoDimensionalShape) {
+				System.out.println(shapeName + ": Area: " + ((TwoDimensionalShape)shape).calcArea());
+			}
+			if (shape instanceof ThreeDimensionalShape) {
+				System.out.println(shapeName + ": SurfaceArea: " + ((ThreeDimensionalShape)shape).calcArea());
+				System.out.println(shapeName + ": Volume: " + ((ThreeDimensionalShape)shape).calcVolume());
+			}
 		}
 	}
 }
